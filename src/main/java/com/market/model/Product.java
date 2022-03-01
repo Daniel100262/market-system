ba2fec33cd62ef6b6abbf29247e1ef5b83a3e449
daniel100262 <daniel100262@hotmail.com>
2022-03-01 19:21:57 -0300

    Added product and category

---
+++ src/main/java/com/market/model/Product.java
@@ -0,0 +1,63 @@
+package com.market.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String name;
+	
+	private String description;
+	
+	private String price;
+	
+	private Category category;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+}
